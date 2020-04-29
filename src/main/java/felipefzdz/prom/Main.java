eec65f265ca37ad275de272cab1fbeca41ea0949
Felipe Fernández <felipe@gradle.com>
2020-04-29 18:38:32 +0100

    Make queueCount configurable.

--- src/main/java/felipefzdz/prom/Main.java
+++ src/main/java/felipefzdz/prom/Main.java
@@ -3,12 +3,18 @@ package felipefzdz.prom;
 import com.codahale.metrics.MetricRegistry;
 import ratpack.dropwizard.metrics.DropwizardMetricsModule;
 import ratpack.dropwizard.metrics.MetricsPrometheusHandler;
+import ratpack.form.Form;
 import ratpack.guice.Guice;
+import ratpack.http.Status;
 import ratpack.server.RatpackServer;
 
-import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
+    public static AtomicInteger queueCount = new AtomicInteger(3);
+
     public static void main(String... args) throws Exception {
         RatpackServer.start(server -> server
             .registry(r -> Guice.registry(b -> {
@@ -18,15 +24,18 @@ public class Main {
                 metrics.register("queueCount", new com.codahale.metrics.Gauge<Integer>() {
                     @Override
                     public Integer getValue() {
-                        return Integer.valueOf(Optional.ofNullable(System.getenv("QUEUE_COUNT")).orElse("3"));
+                        return queueCount.get();
                     }
                 });
 
             }).apply(r))
             .handlers(chain -> chain
                 .get("admin/metrics-report", new MetricsPrometheusHandler())
+                .post("admin/queue-count", ctx -> ctx.parse(Form.class).then(f -> {
+                    queueCount.set(parseInt(f.get("queueCount")));
+                    ctx.getResponse().status(Status.OK).send();
+                }))
             )
         );
-
     }
 }
